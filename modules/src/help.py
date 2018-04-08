ebb0d8f46d421f3778cc2b042af5df38d4697359
Peter Makafan <makafanpeter@gmail.com>
2018-04-08 01:56:20 +0100

    Update help.py

--- modules/src/help.py
+++ modules/src/help.py
@@ -4,7 +4,7 @@ from templates.text import TextTemplate
 
 
 def process(input, entities=None):
-    help = 'Hi there! I\'m Jarvis, your personal assistant.'
+    help = 'Hi there! I\'m Caroline, your personal assistant.'
     if entities is not None:
         if 'sender' in entities and 'first_name' in entities['sender']:
             sender_name = entities['sender']['first_name']
